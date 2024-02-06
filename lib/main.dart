1285d2be55450dd481d3dad32ff6f22d6f32d676
Danila Verbinskiy <work.danv4656@gmail.com>
2024-02-07 01:46:58 +0200

    Refactor routing using GoRouter

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notee/bloc/note_bloc.dart';
-import 'package:notee/pages/add_note_page.dart';
-import 'package:notee/pages/home_page.dart';
-import 'package:notee/pages/view_edit_note_page.dart';
+import 'package:notee/router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,19 +20,14 @@ class NoteeApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => NoteBloc(),
-      child: MaterialApp(
+      child: MaterialApp.router(
         debugShowCheckedModeBanner: false,
         title: 'Notee',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
         darkTheme: ThemeData.dark(),
-        initialRoute: '/',
-        routes: {
-          '/': (context) => const HomePage(),
-          '/add': (context) => const AddNotePage(),
-          '/view_edit': (context) => const ViewEditNotePage(),
-        },
+        routerConfig: router,
       ),
     );
   }
