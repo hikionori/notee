9f376c54990eef327f4186ac8c3139a7c5662f8e
Danila Verbinskiy <work.danv4656@gmail.com>
2024-02-07 14:57:22 +0200

    Refactor NoteBloc event handling

--- lib/bloc/note_bloc.dart
+++ lib/bloc/note_bloc.dart
@@ -8,38 +8,72 @@ part 'note_event.dart';
 part 'note_state.dart';
 
 class NoteBloc extends Bloc<NoteEvent, NoteState> {
-  final NoteStore _noteStore = NoteStore();
+
+  /// The code you provided is the constructor of the `NoteBloc` class.
+  final NoteStore noteStore = NoteStore();
+
   NoteBloc() : super(NoteInitial()) {
     on<NoteEvent>((event, emit) {
-      if (event is LoadNotesEvent) {
-        emit(NoteLoading());
-        emit(NoteLoaded(_noteStore.notes
-          ..sort((a, b) => b.createdAt.compareTo(a.createdAt))));
-      } else if (event is AddNoteEvent) {
-        _noteStore.add(event.title, event.content);
-        emit(NoteLoaded(_noteStore.notes
-          ..sort((a, b) => b.createdAt.compareTo(a.createdAt))));
-      } else if (event is UpdateNoteEvent) {
-        _noteStore.update(event.id, event.title, event.content);
-        emit(NoteLoaded(_noteStore.notes
-          ..sort((a, b) => b.createdAt.compareTo(a.createdAt))));
-      } else if (event is DeleteNoteEvent) {
-        _noteStore.delete(event.id);
-        // sort by last created
-        emit(NoteLoaded(_noteStore.notes
-          ..sort((a, b) => b.createdAt.compareTo(a.createdAt))));
-      } else if (event is SearchNoteEvent) {
-        emit(NoteLoaded(_noteStore.search(event.query)));
-      } else if (event is GetNoteEvent) {
-        final note = _noteStore.get(event.id);
-        if (note != null) {
-          emit(NoteLoaded([note]));
-        } else {
-          emit(NoteError('Note not found'));
-        }
-      } else {
-        emit(NoteError('Unknown event'));
+      switch (event.runtimeType) {
+        case LoadNotesEvent:
+          _loadNotes(emit);
+          break;
+        case AddNoteEvent:
+          _addNoteEvent(event as AddNoteEvent, emit);
+          break;
+        case UpdateNoteEvent:
+          _updateNoteEvent(event as UpdateNoteEvent, emit);
+          break;
+        case DeleteNoteEvent:
+          _deleteNoteEvent(event as DeleteNoteEvent, emit);
+          break;
+        case SearchNoteEvent:
+          _searchNoteEvent(event as SearchNoteEvent, emit);
+          break;
+        case GetNoteEvent:
+          _getNoteEvent(event as GetNoteEvent, emit);
+          break;
+        default:
+          emit(NoteError('Unknown event'));
       }
     });
   }
+
+  void _loadNotes(Emitter<NoteState> emit) {
+    emit(NoteLoading());
+    emit(NoteLoaded(noteStore.notes
+      ..sort((a, b) => b.createdAt.compareTo(a.createdAt))));
+  }
+
+  void _addNoteEvent(AddNoteEvent event, Emitter<NoteState> emit) {
+    noteStore.add(event.title, event.content);
+    emit(NoteLoaded(noteStore.notes
+      ..sort((a, b) => b.createdAt.compareTo(a.createdAt))));
+  }
+
+  void _updateNoteEvent(UpdateNoteEvent event, Emitter<NoteState> emit) {
+    noteStore.update(event.id, event.title, event.content);
+    emit(NoteLoaded(noteStore.notes
+      ..sort((a, b) => b.createdAt.compareTo(a.createdAt))));
+  }
+
+  void _deleteNoteEvent(DeleteNoteEvent event, Emitter<NoteState> emit) {
+    noteStore.delete(event.id);
+    emit(NoteLoaded(noteStore.notes
+      ..sort((a, b) => b.createdAt.compareTo(a.createdAt))));
+  }
+
+  void _searchNoteEvent(SearchNoteEvent event, Emitter<NoteState> emit) {
+    emit(NoteLoaded(noteStore.search(event.query)));
+  }
+
+  void _getNoteEvent(GetNoteEvent event, Emitter<NoteState> emit) {
+    final note = noteStore.get(event.id);
+    if (note != null) {
+      emit(NoteLoaded([note]));
+    } else {
+      emit(NoteError('Note not found'));
+    }
+  }
+
 }
